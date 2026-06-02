3e3eb9a3c7f180e341151d0ef3a58f2509895f6d
Eliel Kwesi Agbeke <brighteliel@gmail.com>
2026-06-02 22:40:41 +0000

    feat: add firebase and ollama configuration modules and ignore .env files

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'AIzaSyBv9aeIJ2wGXAuzIt46mvC6s_5mkRIzXVg',
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'grocare-c77c6.firebaseapp.com',
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'grocare-c77c6',
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'grocare-c77c6.firebasestorage.app',
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '73341031855',
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '1:73341031855:web:4e37dc5b050262ef028b5a',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 export const ollamaConfig = {
